1304456189bc71b045a6b9053df29700ef227b6b
MMoroney <email": null>
2017-08-12 12:54:07 -0500

    Write registration page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+get '/users/new' do
+  erb :'users/new'
+end
+
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect '/sessions/new'
+  else
+    @errors = @user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'/users/show'
+end
--- db/migrate/20170812122539_create_users.rb
+++ db/migrate/20170812122539_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username, presence: true, limit: 20
-      t.string :email, presence: true, limit: 25
-      t.string :password_hash, presence: true, limit: 20
+      t.string :username, presence: true
+      t.string :email, presence: true
+      t.string :password_hash, presence: true
 
       t.timestamps
     end

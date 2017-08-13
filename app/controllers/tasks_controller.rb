a39fcfc3141679c5729e56a10e955cd8d58f60df
MMoroney <email": null>
2017-08-12 19:12:11 -0500

    WIP creating task object with associated guest_id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,1 +1,14 @@
+get '/tasks/new' do
+  erb :'tasks/new'
+end
 
+post '/tasks' do
+  #how to pass a guest id from tasks/new to here?
+  @task = Task.new(params[:task])
+
+  if @task.save
+    redirect '/tasks'
+  else
+    erb :'tasks/new'
+  end
+end

141fc0a0713965fa44fc5d1193412c6aded6e717
MMoroney <email": null>
2017-08-17 13:52:03 -0500

    Task assignment form updated to include guest's phone number, will create a new guest if they don't exist already

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,31 @@
 get '/:id/tasks/new' do
   @cleaning = Cleaning.find(params[:id])
+#  @guests = @cleaning.guests
+#  @tasks = @cleaning.tasks
+
   erb :'tasks/new'
 end
 
 post '/:id/tasks' do
   task_hash = params[:task]
   guest_hash = params[:guest]
-  @guest = Guest.where({first_name: guest_hash[:first_name], last_name: guest_hash[:last_name]})
 
+  #finds an existing guest, if there is one
+  @guest = Guest.where({first_name: guest_hash[:first_name], last_name: guest_hash[:last_name], number: guest_hash[:number]})
+
+  #if the guest doesn't already exist, make a new one and save it
+  if @guest.empty?
+    @guest = Guest.new({first_name: guest_hash[:first_name], last_name: guest_hash[:last_name], number: guest_hash[:number]})
+    @guest.save
+  end
+
+  #reassign @guest to the guest that was just saved.
+  @guest = Guest.where({first_name: guest_hash[:first_name], last_name: guest_hash[:last_name], number: guest_hash[:number]})
+
+  #make a new task object associated with the guest.
   @task = Task.new({description: task_hash[:description], guest_id: @guest[0].id})
 
+  #find the work party that we're current assigning tasks for.
   @cleaning = Cleaning.find(params[:id])
 
   if @task.save
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  // $(".assign-task").on("submit", function(){
+  //   event.preventDefault();
+
+  //   var $data = $(this).serialize();
+  //   var $url = $(this).attr("action");
+  //   var $method = $(this).attr("method");
+
+  //   $.ajax({
+  //     type: $method,
+  //     url: $url,
+  //     data: $data
+  //   })
+  //   .done(function(response){
+  //     console.log(response);
+  //   })
+
+  // })
 });

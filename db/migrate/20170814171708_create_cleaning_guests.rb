e1cfcb68bebe689549748244242ff05ebd624923
MMoroney <email": null>
2017-08-18 13:47:54 -0500

    Minor notes to self

--- db/migrate/20170814171708_create_cleaning_guests.rb
+++ db/migrate/20170814171708_create_cleaning_guests.rb
@@ -1,6 +1,7 @@
 class CreateCleaningGuests < ActiveRecord::Migration
   def change
     create_table :cleaning_guests do |t|
+      #combination of the two following numbers must not be repeated.
       t.integer :cleaning_id
       t.integer :guest_id
 
